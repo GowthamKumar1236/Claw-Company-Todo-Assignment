3104cc9c73349e117ed09c2a1f5e6e6d7a838edd
Gowtham Kumar <gowthamkumar.p1236@gmail.com>
2024-08-03 00:56:36 +0530

    solution

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [token, setToken] = useState('');
+    const [todos, setTodos] = useState([]);
+    const [description, setDescription] = useState('');
+    const [status, setStatus] = useState('');
+
+    const register = async () => {
+        await axios.post('http://localhost:5000/register', { username, password });
+    };
+
+    const login = async () => {
+        const response = await axios.post('http://localhost:5000/login', { username, password });
+        setToken(response.data.token);
+    };
+
+    const fetchTodos = async () => {
+        const response = await axios.get('http://localhost:5000/todos', {
+            headers: { Authorization: token }
+        });
+        setTodos(response.data);
+    };
+
+    const createTodo = async () => {
+        await axios.post('http://localhost:5000/todos', { description, status }, {
+            headers: { Authorization: token }
+        });
+        fetchTodos();
+    };
+
+    useEffect(() => {
+        if (token) fetchTodos();
+        // eslint-disable-next-line
+    }, [token]);
+
+    return (
+        <div>
+            <h1>To-Do App</h1>
+            <div>
+                <h2>Register</h2>
+                <input type="text" placeholder="Username" value={username} onChange={e => setUsername(e.target.value)} />
+                <input type="password" placeholder="Password" value={password} onChange={e => setPassword(e.target.value)} />
+                <button onClick={register}>Register</button>
+            </div>
+            <div>
+                <h2>Login</h2>
+                <input type="text" placeholder="Username" value={username} onChange={e => setUsername(e.target.value)} />
+                <input type="password" placeholder="Password" value={password} onChange={e => setPassword(e.target.value)} />
+                <button onClick={login}>Login</button>
+            </div>
+            {token && (
+                <div>
+                    <h2>Create To-Do</h2>
+                    <input type="text" placeholder="Description" value={description} onChange={e => setDescription(e.target.value)} />
+                    <input type="text" placeholder="Status" value={status} onChange={e => setStatus(e.target.value)} />
+                    <button onClick={createTodo}>Create</button>
+                    <h2>Your To-Dos</h2>
+                    <ul>
+                        {todos.map(todo => (
+                            <li key={todo.id}>{todo.description} - {todo.status}</li>
+                        ))}
+                    </ul>
+                </div>
+            )}
+        </div>
+    );
+};
 
 export default App;
